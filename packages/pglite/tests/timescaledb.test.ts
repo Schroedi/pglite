02ecd14d489887d475bdfae65cb1b153e0ea77cd
Christoph Schröder <cs@keksdev.de>
2026-01-05 15:31:39 +0100

    add IOT sample from timescale documentation

--- packages/pglite/tests/timescaledb.test.ts
+++ packages/pglite/tests/timescaledb.test.ts
@@ -101,5 +101,68 @@ await testEsmCjsAndDTC(async (importType) => {
       )
       expect(Number(buckets.rows[1].avg_value)).toBe(21)
     })
+
+    it('IOT sample', async () => {
+      const pg = new PGlite({
+        extensions: {
+          timescaledb,
+        },
+      })
+
+      await pg.exec('CREATE EXTENSION IF NOT EXISTS timescaledb;')
+
+      // await pg.exec('SELECT timescaledb_pre_restore();');
+      // await pg.exec('SELECT timescaledb_post_restore();');
+
+      await pg.exec(`
+        CREATE TABLE sensors(
+          id SERIAL PRIMARY KEY,
+          type VARCHAR(50),
+          location VARCHAR(50)
+        );
+      `)
+      await pg.exec(`
+        CREATE TABLE sensor_data (
+          time TIMESTAMPTZ NOT NULL,
+          sensor_id INTEGER,
+          temperature DOUBLE PRECISION,
+          cpu DOUBLE PRECISION,
+          FOREIGN KEY (sensor_id) REFERENCES sensors (id)
+        ) WITH (
+          tsdb.hypertable
+        );
+      `)
+
+      await pg.exec(`
+        INSERT INTO sensors (type, location) VALUES
+          ('a','floor'),
+          ('a', 'ceiling'),
+          ('b','floor'),
+          ('b', 'ceiling');
+        `)
+
+        await pg.exec(`
+          INSERT INTO sensor_data (time, sensor_id, cpu, temperature)
+          SELECT
+            time,
+            sensor_id,
+            random() AS cpu,
+            random()*100 AS temperature
+          FROM generate_series(now() - interval '24 hour', now(), interval '5 minute') AS g1(time), generate_series(1,4,1) AS g2(sensor_id);
+        `)
+
+        const res = await pg.query(`
+          SELECT
+            sensors.location,
+            time_bucket('30 minutes', time) AS period,
+            AVG(temperature) AS avg_temp,
+            last(temperature, time) AS last_temp,
+            AVG(cpu) AS avg_cpu
+          FROM sensor_data JOIN sensors on sensor_data.sensor_id = sensors.id
+          GROUP BY period, sensors.location;
+        `)
+
+        console.log('res:', res.rows)
+    })
   })
 })
